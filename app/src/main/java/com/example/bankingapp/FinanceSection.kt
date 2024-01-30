2e0b39f121455160145e06d665dbb005259c36bb
pranushareddy <pranusha.sweety93@gmail.com>
2024-01-30 18:11:54 -0500

    banking app in kotlin and jetpack compose

---
+++ app/src/main/java/com/example/bankingapp/FinanceSection.kt
@@ -0,0 +1,117 @@
+package com.example.bankingapp
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AutoGraph
+import androidx.compose.material.icons.rounded.MonetizationOn
+import androidx.compose.material.icons.rounded.StarHalf
+import androidx.compose.material.icons.rounded.Wallet
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bankingapp.data.Finances
+import com.example.bankingapp.ui.theme.BlueStart
+import com.example.bankingapp.ui.theme.GreenStart
+import com.example.bankingapp.ui.theme.OrangeStart
+import com.example.bankingapp.ui.theme.PurpleStart
+
+val financeList = listOf(
+    Finances(
+        icon = Icons.Rounded.StarHalf,
+        title = "My\nBusiness",
+        background = OrangeStart
+    ),
+    Finances(
+        icon = Icons.Rounded.Wallet,
+        title = "My\nWallet",
+        background = BlueStart
+    ),
+    Finances(
+        icon = Icons.Rounded.AutoGraph,
+        title = "Finance\nAnalysis",
+        background = PurpleStart
+    ),
+    Finances(
+        icon = Icons.Rounded.MonetizationOn,
+        title = "My\nTransactions",
+        background = GreenStart
+    )
+)
+
+@Preview
+@Composable
+fun FinanceSection() {
+    Column {
+        Text(
+            text = "Finance",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+        LazyRow {
+            items(financeList.size) { index ->
+                FinanceItem(index)
+            }
+        }
+    }
+}
+
+@Composable
+fun FinanceItem(
+    index: Int
+) {
+    val finance = financeList[index]
+    var lastItemPaddingEnd = 0.dp
+    if (index == financeList.size - 1) {
+        lastItemPaddingEnd = 16.dp
+    }
+    Box(modifier = Modifier.padding(start = 16.dp, end = lastItemPaddingEnd)) {
+
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(25.dp))
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+                .size(120.dp)
+                .clickable { }
+                .padding(13.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(finance.background)
+                    .padding(6.dp)
+            ) {
+                Icon(
+                    imageVector = finance.icon,
+                    contentDescription = finance.title,
+                    tint = Color.White
+                )
+            }
+            Text(
+                text = finance.title,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bankingapp/data/Finances.kt
@@ -0,0 +1,10 @@
+package com.example.bankingapp.data
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Finances(
+    val icon: ImageVector,
+    val title: String,
+    val background: Color
+)
